3022a7b54082ebdee4f95bcfa7ed11c3d98b4d60
Rachit-Mangla <124048734+Rachit-Mangla@users.noreply.github.com>
2025-06-23 17:46:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,10 +33,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
